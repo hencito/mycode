912e187e92bb9bcb10d54b459c8db57e6eaa8198
hencito <hencyperez@gmail.com>
2022-11-08 16:48:44 +0000

    lab 18 challenge

---
+++ lab_input/challenge_print_input.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+user_input_name = input("Hello, what is your name? ")
+user_input_dayofweek= input("What is today's day of the week? ")
+print("Hello "+user_input_name+"!"+" Happy "+ user_input_dayofweek + "!")
